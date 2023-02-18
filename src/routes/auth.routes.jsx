844250635c3da97f1585c1d2014dd46d30a85f4c
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-18 06:27:57 -0300

    setup e criação da rota signup

---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+import { SignUp } from "../pages/SignUp";
+
+export function AuthRoutes() {
+  return (
+    <Routes>
+      <Route path="/register" element={<SignUp />} />
+    </Routes>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,5 @@
+export default {
+  COLORS: {
+    BLACK: "#000000",
+  },
+};
